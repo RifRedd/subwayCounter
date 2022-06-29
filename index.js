90291d4de187d1e97f4bdc5bac9afba2f60e5216
Reese Rifredi <reese.rifredi@gmail.com>
2022-06-29 11:45:24 -0700

    various updates

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-document.getElementById("count-el").innerText=5
+let count = 0
+
+function increment() {
+    count = count + 1
+    count = document.getElementById("count-el").innerHTML=count
+}
+
+function save() {
+    console.log(count)
+}
+
+let message = "You have three new notifications"
+console.log(message)
